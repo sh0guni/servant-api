97afbaf45e09f78fb936732abcac387f81448ac7
Jesse Korpela <donburij@gmail.com>
2019-04-18 23:16:12 +0900

    add simple users api

---
+++ ChangeLog.md
@@ -0,0 +1,3 @@
+# Changelog for hems-haskell
+
+## Unreleased changes
---
+++ src/Api.hs
@@ -0,0 +1,43 @@
+{-# LANGUAGE DataKinds #-}
+{-# LANGUAGE DeriveGeneric #-}
+{-# LANGUAGE TypeOperators #-}
+
+module Api where
+
+import Data.Aeson
+import Data.Text
+import Data.Time (UTCTime)
+import GHC.Generics
+import Servant
+import Servant.API
+import Network.Wai
+import Network.Wai.Handler.Warp
+
+type UserAPI = "users" :> Get '[JSON] [User]
+
+data SortBy = Age | Name
+
+data User = User {
+    name :: String,
+    age :: Int,
+    email :: String
+} deriving (Eq, Show, Generic)
+
+instance ToJSON User
+
+users =
+  [ User "Isaac Newton"    372 "isaac@newton.co.uk"
+  , User "Albert Einstein" 136 "ae@mc2.org"
+  ]
+
+server1 :: Server UserAPI
+server1 = return users
+
+userAPI :: Proxy UserAPI
+userAPI = Proxy
+
+app1 :: Application
+app1 = serve userAPI server1
+
+start :: IO ()
+start = run 3000 app1
